a87e39173a028802c0c228889c1cfe318023dea8
Duncan Harvey <duncanpharvey@gmail.com>
2020-04-19 10:01:56 -0400

    adds puppeteer click tests and refactors jsdom click tests

--- spec/spec.js
+++ spec/spec.js
@@ -1,39 +1,89 @@
 const Reporter = require("jasmine-console-reporter");
-jasmine.getEnv().addReporter(new Reporter()); // displays test summary nicely in console
+jasmine.getEnv().addReporter(new Reporter()); // display test summary nicely in console
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom; // use DOM in node
+const { JSDOM } = require("jsdom");
+const puppeteer = require('puppeteer');
+require('../app.js');
+require('dotenv').config();
 
-describe("Click Tests", () => {
-  beforeEach(async function() {
-    await JSDOM.fromFile("./index.html", { runScripts: "dangerously", resources: "usable" }).then(dom => { // wait for dom to load from file before running tests
-      window = dom.window;
-    });
-    await new Promise(resolve => window.addEventListener("load", resolve)); // wait for external scripts to finish before running tests
-    document = window.document; // global document variable reset for each test run
-    button = document.getElementById("the-button");
-    text = document.getElementById("hidden-text");
+describe("Puppeteer Click Tests", () => {
+  beforeEach(async function () {
+    this.browser = await puppeteer.launch({ headless: true });
+    this.page = await this.browser.newPage();
+    await this.page.goto(`http://localhost:${process.env.PORT}/`);
+    this.handle = await this.page.evaluateHandle(() => ({ document }));
+    const properties = await this.handle.getProperties();
+    this.documentHandle = properties.get('document');
+  });
+
+  afterEach(async function () {
+    await this.handle.dispose();
+    await this.browser.close();
   });
 
   // no clicks
-  it("should hide text on page load", () => {
-    expect(document.getElementById("hidden-text").style.display).toEqual("none");
-  })
+  it("should hide text on page load", async function () {
+    var displayStatus = await this.page.evaluate(document => {
+      return document.getElementById("hidden-text").style.display;
+    }, this.documentHandle);
+    expect(displayStatus).toEqual("none");
+  });
 
   // one click
-  it("should toggle text display status when clicked once", () => {
-    var before = text.style.display;
-    button.click();
-    var after = text.style.display;
-    expect(before).not.toEqual(after);
-  })
+  it("should toggle text display status when clicked once", async function () {
+    var displayStatus = await this.page.evaluate(document => {
+      var text = document.getElementById("hidden-text");
+      var button = document.getElementById("the-button");
+      var before = text.style.display;
+      button.click();
+      return { before: before, after: text.style.display };
+    }, this.documentHandle);
+    expect(displayStatus.before).not.toEqual(displayStatus.after);
+  });
 
   // two clicks
-  it("should have original text display status when clicked twice", () => {
-    var before = text.style.display;
-    button.click();
-    button.click();
-    var after = text.style.display;
-    expect(before).toEqual(after);
-  })
+  it("should have original text display status when clicked twice", async function () {
+    var displayStatus = await this.page.evaluate(document => {
+      var text = document.getElementById("hidden-text");
+      var button = document.getElementById("the-button");
+      var before = text.style.display;
+      button.click();
+      button.click();
+      return { before: before, after: text.style.display };
+    }, this.documentHandle);
+    expect(displayStatus.before).toEqual(displayStatus.after);
+  });
+
+});
+
+
+describe("JSDOM Click Tests", () => {
+  beforeEach(async function () {
+    await JSDOM.fromFile("./index.html", { runScripts: "dangerously", resources: "usable" }).then(dom => { // wait for dom to load from file before running tests
+      this.window = dom.window;
+    });
+    await new Promise(resolve => this.window.addEventListener("load", resolve)); // wait for external scripts to finish before running tests
+    var document = this.window.document;
+    this.button = document.getElementById("the-button");
+    this.text = document.getElementById("hidden-text");
+    this.before = this.text.style.display;
+  });
+
+  // no clicks
+  it("should hide text on page load", async function () {
+    expect(this.before).toEqual("none");
+  });
+
+  // one click
+  it("should toggle text display status when clicked once", async function() {
+    this.button.click();
+    expect(this.before).not.toEqual(this.text.style.display);
+  });
+
+  // two clicks
+  it("should have original text display status when clicked twice", async function() {
+    this.button.click();
+    this.button.click();
+    expect(this.before).toEqual(this.text.style.display);
+  });
 });
